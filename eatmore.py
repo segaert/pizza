41375a104d0c3291548db9e6fd934b07c5d0e6f1
Ludovik Segaert <149730230+segaert@users.noreply.github.com>
2024-03-28 17:16:40 +0100

    Create eatmore.py

---
+++ eatmore.py
@@ -0,0 +1,33 @@
+def enqueue(queue,item):
+    queue.append(item)
+
+def dequeue(queue):
+    return queue.pop(0)
+    
+def peek(queue):
+    return queue[0]
+
+def isempty(queue):
+    return len(queue) == 0
+    
+def size(queue):
+    return len(queue)
+
+pizzaqueue = []
+
+enqueue(pizzaqueue,"pizza hawaii")
+enqueue(pizzaqueue,"pizza calzone")
+enqueue(pizzaqueue,"pizza calzone")
+
+print('Eerstvolgende aan de beurt: ' + peek(pizzaqueue))
+print(str(size(pizzaqueue)) + ' pizza''s in de wachrij')
+
+#Opdracht herschrijf onderstaande lijnen in een while loop
+currentpizza = dequeue(pizzaqueue)
+print(currentpizza + ' aan het klaarmaken')
+
+currentpizza = dequeue(pizzaqueue)
+print(currentpizza + ' aan het klaarmaken')
+
+currentpizza = dequeue(pizzaqueue)
+print(currentpizza + ' aan het klaarmaken')
