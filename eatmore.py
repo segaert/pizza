54dd139576768146a6330a6087d65f39e147c07f
Ludovik Segaert <149730230+segaert@users.noreply.github.com>
2024-05-04 13:49:11 +0200

    Update eatmore.py

--- eatmore.py
+++ eatmore.py
@@ -1,11 +1,10 @@
+import time
+
 def enqueue(queue,item):
     queue.append(item)
 
 def dequeue(queue):
     return queue.pop(0)
-    
-def peek(queue):
-    return queue[0]
 
 def isempty(queue):
     return len(queue) == 0
@@ -19,18 +18,24 @@ enqueue(pizzaqueue,"pizza hawaii")
 enqueue(pizzaqueue,"pizza calzone")
 enqueue(pizzaqueue,"pizza calzone")
 
-print('Eerstvolgende aan de beurt: ' + peek(pizzaqueue))
 print(str(size(pizzaqueue)) + ' pizza''s in de wachrij')
 
-#Opdracht herschrijf onderstaande lijnen in een while loop
-currentpizza = dequeue(pizzaqueue)
-print(currentpizza + ' aan het klaarmaken')
-sleep(2)
+#Opdracht 1 schrijf een functie om één pizza klaar te maken
+#           met als parameter de pizzaqueue en gebruik dan de 
+#           functie om onderstaande drie blokken te herschrijven in één lijn
+#           Kies een gepaste naam voor de functie
 
 currentpizza = dequeue(pizzaqueue)
 print(currentpizza + ' aan het klaarmaken')
-sleep(2)
+time.sleep(2)
 
 currentpizza = dequeue(pizzaqueue)
 print(currentpizza + ' aan het klaarmaken')
-sleep(2)
+time.sleep(2)
+
+currentpizza = dequeue(pizzaqueue)
+print(currentpizza + ' aan het klaarmaken')
+time.sleep(2)
+
+#Opdracht 2 herschrijf de drie lijnen in een loop
+#           denk na of je een for of een while gebruikt
